e8b2011313a9de5f79e9e12deeb9860ed0eac60c
Anurag Roy <89125578+binarybeast880@users.noreply.github.com>
2022-05-22 23:32:32 +0530

    Add files via upload

---
+++ var.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let var1 = 10;  //     let var1:i8 = 10;
+    let var2 = 10.10; //    let var2:f32 = 10.10;
+    let var3 = true;  //     let var3:bool = true;
+    let var4 = 'A';   //       let var4:char = 'A';
+    println!(" {} ", var1);
+    println!(" {} ", var2);
+    println!(" {} ", var3);
+    println!(" {} ", var4);
+}
